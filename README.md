2491441ff6e35cd02c08f57bd1a9b8b097461732
Christian Ashby <git@cashby.me.uk>
2016-02-23 22:09:17 +0000

    Processing data from source JSON to GeoJSON thence to KML using Node module tokml

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # scapi
+
+Prerequisites
+* Node.js (Obviously)
+
+Installation
+* npm install
+
+Usage
+* For now, running the node app.js gives a KML file as the response to /api/superchargers/geo
+
+Thanks
+* @tom - original JSON parsing concept here: https://github.com/tomfitzherbert/scapi
+
+Changelog
+* 0.0.1 - Tom's original version (Hello World JSON parsing)
+* 0.0.2 - Christian's additions to parse geo information into KML format
--- controllers/superchargers.js
+++ controllers/superchargers.js
@@ -1,26 +1,50 @@
 var request = require("request")
-
-
-
+var xml = require("xml")
+var tokml = require("tokml")
 
 /**
  * GET /api/superchargers/geo
  * Get the geo details of a supercharger 
  */
-exports.getGeo = function(req, res) {
+exports.getGeo = function (req, res) {
 
-  var url = 'http://supercharge.info/service/supercharge/allSites'
+    var url = 'http://supercharge.info/service/supercharge/allSites'
 
-  request ({
-    url: url,
-    json: true
-  }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var superchargers = body
-      // insert logic to do whatever you want with the supercharger data 
-      res.json(superchargers[0]);
-    } else {
-      console.log('Something went wrong :(')
-    }
-  })
+    // Containers for GeoJSON data
+    var features = [];
+    var featureCollection = {
+        type: "FeatureCollection",
+        features: features
+    };
+
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var superchargers = body;
+
+            for (var sc in superchargers) {
+                features.push({
+                    type: "Feature",
+                    geometry: {
+                        type: "Point",
+                        coordinates: [superchargers[sc].gps.longitude, superchargers[sc].gps.latitude]
+                    },
+                    properties: {
+                        id: superchargers[sc].id,
+                        //locationId: JSON.stringify(superchargers[sc].locationId), TODO: Why does this break tokml?
+                        address: JSON.stringify(superchargers[sc].address),
+                        name: superchargers[sc].name,
+                        stalls: superchargers[sc].stallCount
+                    }
+                });
+            }
+
+            res.set("Content-Type", "text/xml");
+            res.send(tokml(featureCollection));
+        } else {
+            console.log('Something went wrong :(')
+        }
+    })
 };
